040bfb8fa4d814c8ddbf71d121c3015c9199f1ac
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2022-03-14 10:01:36 +0800

    Add files via upload

---
+++ Tugas_Musdalifa.py
@@ -0,0 +1,53 @@
+#bolean
+a = 3>1 #true
+print(a)
+b = 3<1 #false
+
+#string
+nama ="musdalifa"
+print(nama)
+
+#intejer
+angka1 =13
+print(angka1)
+
+#tipe data float
+angka2 =20.02
+print(angka2)
+
+#operator aritmatika
+#-penjumlahan (+)
+#-pengurangan (-)
+#-perkalian (*)
+#-pembagian (/)
+#-sisa bagi (%)
+#-pangkat(**)
+#-pembagian bulat (//)
+
+nilai1 =5
+nilai2 =6
+print(nilai1 + nilai2)
+
+nilai1 =10
+nilai2 =5
+print(nilai1 -nilai2)
+
+nilai1 =12
+nilai2 =4
+print(nilai1 * nilai2)
+
+nilai1 =15
+nilai2 =3
+print(nilai1/nilai2)
+
+nilai1 =23
+nilai2 =4
+print(nilai1%nilai2)
+
+nilai1 =70
+nilai2 =3
+print(nilai1 ** nilai2)
+
+nilai1 =50
+nilai2 =25
+print(nilai1//nilai2)
